01796332f9d22e9857a2553b3cbc07e3c034282b
OrangeBacon <computer.backup.15@gmail.com>
2021-05-05 11:58:48 +0100

    Read commandline arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,118 @@
+use std::{
+    fmt::{self, Display},
+    fs,
+    io::{self, Read},
+};
+
+use anyhow::Result;
+use clap::{clap_app, crate_authors, crate_description, crate_version};
+use thiserror::Error;
+
+/// Errors encountered while interpreting the input arguments
+#[derive(Debug, Error)]
+enum InputError {
+    #[error("Encountered errors while reading files:\n{files}")]
+    FileError { files: IoErrorVec },
+
+    #[error("No input was provided")]
+    NoInput,
+}
+
+/// New type wrapper to provide display impl
+#[derive(Debug)]
+struct IoErrorVec(Vec<io::Error>);
+
+impl Display for IoErrorVec {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for err in &self.0 {
+            writeln!(f, "{}", err)?;
+        }
+
+        Ok(())
+    }
+}
+
+struct SourceFile {
+    path: Option<String>,
+    content: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        println!("Error:\n{}", e);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    let matches = clap_app!(scheme =>
+        (version: crate_version!())
+        (author: crate_authors!())
+        (about: crate_description!())
+        (@arg input: +multiple "Input files to parse.  If not present uses stdin.")
+        (@arg eval: -e "Interpret the input as source code instead of file names")
+    )
+    .get_matches();
+
+    let mut sources = vec![];
+
+    // try to work out the meaning of the inputs
+    if let Some(file_names) = matches.values_of("input") {
+        // the input is source code
+        if matches.is_present("eval") {
+            sources.push(SourceFile {
+                path: None,
+                content: file_names.fold(String::new(), |a, b| a + b),
+            });
+        } else {
+            // the input is file names
+
+            // any file read errors
+            let mut errors = vec![];
+
+            for file in file_names {
+                // file name "-" == read from stdin
+                if file == "-" {
+                    match get_stdin() {
+                        Ok(input) => sources.push(input),
+                        Err(err) => errors.push(err),
+                    }
+                } else {
+                    // try to interpret as a source file name
+                    match fs::read_to_string(file) {
+                        Ok(input) => sources.push(SourceFile {
+                            path: Some(file.to_string()),
+                            content: input,
+                        }),
+                        Err(err) => errors.push(err),
+                    }
+                };
+            }
+
+            if !errors.is_empty() {
+                Err(InputError::FileError {
+                    files: IoErrorVec(errors),
+                })?;
+            }
+        }
+    } else {
+        sources.push(get_stdin()?);
+    }
+
+    // no input provided, so nothing to do
+    if sources.is_empty() || sources.iter().all(|s| s.content.is_empty()) {
+        Err(InputError::NoInput)?;
+    }
+
+    Ok(())
+}
+
+/// read source code from stdin
+fn get_stdin() -> Result<SourceFile, io::Error> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+    Ok(SourceFile {
+        path: Some("stdin".to_string()),
+        content: input,
+    })
 }
